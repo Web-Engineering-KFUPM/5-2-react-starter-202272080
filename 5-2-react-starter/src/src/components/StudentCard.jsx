6302be285804957dd4d60382e12d6fc03f9f8b49
202272080 <s20227208@kfupm.edu.sa>
2025-09-30 11:46:20 +0300

    student

---
+++ 5-2-react-starter/src/src/components/StudentCard.jsx
@@ -0,0 +1,14 @@
+
+  function StudentCard() {
+    return (
+      <div>
+        <h3>studenName:ALI</h3>
+        <p>studentId: 202272080</p>
+        <p>studentDepartment:c&m</p>
+       
+      </div>
+    );
+  }
+
+  export default StudentCard;
+
